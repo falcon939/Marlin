5bce3ae159f1f3687c53ff1ccdc8786b7ac2cd63
Falcon939 <falcon939+git@gmail.com>
2022-02-13 22:13:20 +0100

    Update the build-date

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Falcon939 13-07-2021, E5P, Octopus)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Falcon939 13-02-2022, E5P, Octopus)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -96,7 +96,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_OCTOPUS_V1_1
 #endif
 
 /**
